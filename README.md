ab848eb7c5dd82aa575beb79b1587d0edafcd3cd
Gabriel <gabrielalejandrorosalesazurdia@gmail.com>
2019-08-04 13:35:03 -0600

    Primer Guardado

---
+++ README.md
@@ -0,0 +1,3 @@
+# VirtualBook Backend
+
+this is the backend of my page and app virtualbook
--- biblioteca_virtualbook_backend/settings.py
+++ biblioteca_virtualbook_backend/settings.py
@@ -169,9 +169,5 @@ JWT_AUTH = {
 
 AUTH_USER_MODEL = 'accounts.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'juanagutixtix@gmail.com'
-EMAIL_HOST_PASSWORD = 'PalaQueChevere'
+import django_heroku
+django_heroku.settings(locals())
