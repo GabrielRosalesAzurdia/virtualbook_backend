e654ad27286e2dacc50105fecc3d1c88b616b026
Gabriel <gabrielalejandrorosalesazurdia@gmail.com>
2019-08-06 16:18:56 -0600

    Primer Guardado

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'password',
             'credit_card',
-            'imagen',
+            'image',
             'address_1',
             'address_2',
             'city',
