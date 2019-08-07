fb6c084f976091fd73c9487cfa96df5151790e54
Gabriel <gabrielalejandrorosalesazurdia@gmail.com>
2019-08-06 18:23:15 -0600

    Primer Guardado

--- accounts/models.py
+++ accounts/models.py
@@ -45,3 +45,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f'{self.email}'
+
+    def nameFile(instance, filename):
+        return '/'.join(['images', str(instance.name), filename])
